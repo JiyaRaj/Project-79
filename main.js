e0f9f74991a727921b8f1727727bac99f5a91852
JiyaRaj <86504048+JiyaRaj@users.noreply.github.com>
2021-12-17 17:17:42 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+menu_list_array = ["Veg Margherita Pizza",
+                        "Chicken Tandoori Pizza",
+                        "Veg Supreme Pizza",
+                        "Paneer Tikka Pizza",
+                        "Deluxe Veggie Pizza",
+                        "Veg Extravaganza Pizza"];
+    
+    function getmenu(){
+        var htmldata="<ol class='menu_list'>";
+        menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+'<li>'+ menu_list_array[i] + '<li>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+        htmldata=htmldata+"</ol>"
+    }
+
+    function add_item(){
+        var htmldata;
+		var imgtags='<img id="im1" src="pizzaImg.png">'
+        var item=document.getElementById("add_item").value;
+        menu_list_array.push(item);
+        menu_list_array.sort();
+        htmldata="<section class='cards'";
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+'<divclass="card">'+ '<img src="pizzaImg.png/>'+ menu_list_array[i]+'<div>';
+        }
+         
+         
+         htmldata=htmldata+"</section>"
+         document.getElementById("display_addmenu").innerHTML=htmldata;
+         document.getElementById("add_item").value="";
+         
+    }
+
+    
+
